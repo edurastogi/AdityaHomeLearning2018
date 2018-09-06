0c23e70611a82542895311740536e76cc5b5aab6
aditya rastogi <edurastogi@gmail.com>
2018-09-06 04:58:00 -0400

    code checkin

--- src/main/java/com/leetcode/PlusOne.java
+++ src/main/java/com/leetcode/PlusOne.java
@@ -1,38 +1,72 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 public class PlusOne {
     public static void main(String[] args) {
-        int[] firstArray = {7,9,9,9,9};
-        int[] plusOne =  new int[firstArray.length +1];
-        int carry =0;
-        int p1 = firstArray.length -1 ;
-        int temp = firstArray[p1] + 1 ;
 
+        PlusOne p = new PlusOne();
+        int[] digits = {9,9,9,9};
+        System.out.println(Arrays.toString(p.plusOne(digits)));
 
+        /*
+        three cases
+        1) there is no carry : add one to last element of the array as as long as it is leas than 10 it is fine
+        2) there is carry: adding one to last element makes sum equal or greater than 10, then we will put reminder
+           in last place and add one in second last position, this will keep repeating until there is no carry
+        3) we are reached at the begining of the last and after adding carry sum is greater than one, then we need to create
+           a new array with one additonal size and then copy the arry
 
-
-
-        for (int i = firstArray.length -1; i >=0 ; i--) {
-            System.out.println(firstArray[i]);
+        int[] digits = {9,9,9,9};
+        System.out.println(Arrays.toString(digits));
+        int carry = 1;
+        int temp;
+        for (int i = digits.length-1; i >=0 ; i--) {
+            if(carry ==0) break;
+            temp = digits[i] + carry;
+            if (temp >= 10) {
+                digits[i] = temp % 10;
+                carry = temp / 10;
+            } else {
+                digits[i] = temp;
+                carry =0;
+            }
         }
-
-
-
-
-
-        /*int[] inputArray = {2,3,7,4,5};
-        System.out.println(Arrays.toString(inputArray));
-        reverseIntArray(inputArray);
-        System.out.println(Arrays.toString(inputArray));*/
+        if(carry !=0){
+            int[] plusOne =  new int[digits.length +1];
+            plusOne[0] = carry;
+            for (int i = 0; i < digits.length ; i++) {
+                plusOne[i+1] = digits[i];
+            }
+            digits = plusOne;
+        }
+        System.out.println(Arrays.toString(digits)); */
     }
 
-    public static void reverseIntArray(int[] inputArray){
-        if(inputArray==null || inputArray.length <=1) return;
-        for(int i = 0; i < inputArray.length / 2; i++)
-        {
-            int temp = inputArray[i];
-            inputArray[i] = inputArray[inputArray.length - i - 1];
-            inputArray[inputArray.length - i - 1] = temp;
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        int temp;
+        for (int i = digits.length-1; i >=0 ; i--) {
+            if(carry ==0) break;
+            temp = digits[i] + carry;
+            if (temp >= 10) {
+                digits[i] = temp % 10;
+                carry = temp / 10;
+            } else {
+                digits[i] = temp;
+                carry =0;
+            }
         }
+        if(carry !=0){
+            int[] plusOne =  new int[digits.length +1];
+            plusOne[0] = carry;
+            for (int i = 0; i < digits.length ; i++) {
+                plusOne[i+1] = digits[i];
+            }
+            digits = plusOne;
+        }
+        return digits;
+
     }
+
 }
