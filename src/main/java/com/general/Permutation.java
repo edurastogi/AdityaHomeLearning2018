a53f9d68c4276175f88342c3ecedbfc325a4bb20
aditya rastogi <edurastogi@gmail.com>
2018-10-03 16:56:12 -0400

    code checkincode checkin

--- src/main/java/com/general/Permutation.java
+++ src/main/java/com/general/Permutation.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Permutation {
     public static void main(String[] args) {
         String a = "abcd";
-        String b = "bpda";
+        String b = "bcda";
         System.out.println(isPermutation(a, b));
     }
 
@@ -19,12 +19,16 @@ public class Permutation {
         String[] aArray = a.split("(?!^)");
         String[] bArray = b.split("(?!^)");
         for (String s : aArray) {
+            //TODO: This will will not work in case of repeating alphabets,
             aMap.put(s, s);
         }
         for (String s : bArray) {
             if (!aMap.containsKey(s)) {
                 isPer = false;
                 break;
+            }else {
+
+                aMap.remove(s);
             }
         }
         return isPer;
---
+++ src/main/java/com/leetcode/Sqrt.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        Sqrt s = new Sqrt();
+        System.out.println(s.mySqrt(8));
+
+    }
+
+    public int mySqrt(int x){
+        int low = 1;
+        int high = 2;
+        while(1==1){
+            if(x >= Math.pow(low,2) && x < Math.pow(high,2) ){
+                break;
+            }
+            else{
+                low++;
+                high++;
+            }
+        }
+        return low;
+    }
+}
