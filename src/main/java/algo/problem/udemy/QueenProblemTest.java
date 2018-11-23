289889aabc0beb9b595eb9e85dd695d97af71887
aditya rastogi <edurastogi@gmail.com>
2018-11-22 23:04:31 -0500

    Queen placement problem

---
+++ src/main/java/algo/problem/udemy/QueenProblemTest.java
@@ -0,0 +1,8 @@
+package algo.problem.udemy;
+
+public class QueenProblemTest {
+    public static void main(String[] args) {
+        QueensProblem problem = new QueensProblem(10);
+        problem.solve();
+    }
+}
