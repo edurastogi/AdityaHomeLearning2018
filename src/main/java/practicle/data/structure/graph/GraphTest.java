20ceb295b0d79477b4c802010c1b075508477847
aditya rastogi <edurastogi@gmail.com>
2018-12-02 19:59:05 -0500

    Basic Graph implementation

---
+++ src/main/java/practicle/data/structure/graph/GraphTest.java
@@ -0,0 +1,27 @@
+package practicle.data.structure.graph;
+
+public class GraphTest {
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph(5);
+
+        myGraph.addEdge(0,1);
+        myGraph.addEdge(0,2);
+        myGraph.addEdge(0,3);
+
+        myGraph.addEdge(1,2);
+        myGraph.addEdge(1,4);
+
+        myGraph.addEdge(2,3);
+
+        myGraph.addEdge(3,1);
+
+        myGraph.addEdge(4,0);
+        myGraph.addEdge(4,3);
+
+        Object[] values = myGraph.adj(0);
+        for(Object val : values){
+            System.out.println(val);
+        }
+    }
+}
