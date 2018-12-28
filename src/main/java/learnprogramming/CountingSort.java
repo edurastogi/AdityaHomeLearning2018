04922a49025e59f40f68cc495fecb74c712f13cd
aditya rastogi <edurastogi@gmail.com>
2018-12-28 16:19:46 -0500

    quick sort

---
+++ src/main/java/learnprogramming/CountingSort.java
@@ -0,0 +1,10 @@
+package learnprogramming;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] intArray = {2,5,9,8,2,8,7,10,4,3};
+        System.out.println(Arrays.toString(intArray));
+    }
+}
