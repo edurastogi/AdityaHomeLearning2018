1db92047f8cff63409176549b9dd2d0d828bb0de
aditya rastogi <edurastogi@gmail.com>
2018-12-30 20:15:14 -0500

    Radix Sort

---
+++ src/main/java/design/principles/SingleResponsibilityPrinciple.java
@@ -0,0 +1,24 @@
+package design.principles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleResponsibilityPrinciple {
+}
+
+class Journal{
+
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+
+    public void addEntry(String text){
+        entries.add("" + (++count) + ": " + text);
+    }
+
+    public void removeEntry(int index){
+        entries.remove(index);
+    }
+
+
+
+}
