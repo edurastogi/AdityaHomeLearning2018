4790568fd5aeee88367af898caaea126014b489e
aditya rastogi <edurastogi@gmail.com>
2019-01-19 08:08:50 -0500

    incremental learning

---
+++ src/main/java/learnprogramming/hashtable/StoredEmployee.java
@@ -0,0 +1,13 @@
+package learnprogramming.hashtable;
+
+import learnprogramming.lists.Employee;
+
+public class StoredEmployee {
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
