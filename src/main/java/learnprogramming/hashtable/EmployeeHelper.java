84e65643069beb33a34fb697ba88b4b721fd9dc4
aditya rastogi <edurastogi@gmail.com>
2019-01-26 03:17:34 -0500

    incremental learning

---
+++ src/main/java/learnprogramming/hashtable/EmployeeHelper.java
@@ -0,0 +1,11 @@
+package learnprogramming.hashtable;
+
+import learnprogramming.lists.Employee;
+
+public class EmployeeHelper{
+
+    public static Employee janeJones = new Employee("Jane", "Jones", 123);
+    public static Employee johnDoe = new Employee("John", "Doe", 4567);
+    public static Employee marySmith = new Employee("Mery", "Smith", 22);
+    public static Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+}
