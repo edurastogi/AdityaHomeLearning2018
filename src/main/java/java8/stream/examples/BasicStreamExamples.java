a306d43e827305fae576323cc4fc37f0f4b788a9
aditya rastogi <edurastogi@gmail.com>
2019-09-05 20:17:34 -0400

    incremental checkins

--- src/main/java/java8/stream/examples/BasicStreamExamples.java
+++ src/main/java/java8/stream/examples/BasicStreamExamples.java
@@ -1,8 +1,10 @@
 package java8.stream.examples;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class BasicStreamExamples{
@@ -17,7 +19,50 @@ public class BasicStreamExamples{
 
         //getting stream, applying filter and the collecting object in list
         List<String> filtered = items.stream()
-                .filter(item ->item.startsWith("o"))
+                .filter(item -> item.startsWith("o"))
                 .collect(Collectors.toList());
+
+        List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+        //example
+        myList
+                .stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+
+        //stream() and parallelStream()
+
+        Arrays.asList("a1", "a2", "a3")
+                .stream()
+                .findFirst()
+                .ifPresent(System.out::println); //a1
+
+        //Stream.of() to create stream without collection
+
+        Stream.of("a1", "a2", "a3")
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        //IntStream,LongStream,DoubleStream
+        IntStream.range(1, 5)
+                .forEach(System.out::println);
+
+        Arrays.stream(new int[]{1, 2, 3, 5})
+                .map(n -> (2 * n + 1))
+                .average()
+                .ifPresent(System.out::println);
+
+        //Converting object stream to primitive stream
+        Stream.of("a1","a2","a3")
+                .map(s -> s.substring(1))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .ifPresent(System.out::println);
+
+
+
+
     }
 }
