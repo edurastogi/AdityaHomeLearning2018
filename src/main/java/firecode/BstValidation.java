e25c82b0f75e031d51f648e914ba536391ef5870
aditya rastogi <edurastogi@gmail.com>
2019-09-25 20:21:07 -0400

    minor improvements

--- src/main/java/firecode/BstValidation.java
+++ src/main/java/firecode/BstValidation.java
@@ -3,10 +3,17 @@ package firecode;
 public class BstValidation{
     /* Given the root node of a Binary Tree, determine if it is a Binary Search Tree.*/
     public static boolean validateBST(TreeNode root){
-        return validateBstWithLimit(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        return validateBstWithLimit(root, null, null);
     }
 
-    public static boolean validateBstWithLimit(TreeNode root, int min, int max){
+
+    public static boolean validateBstWithLimit(TreeNode root, Integer min, Integer max){
+        if(root == null) return true;
+        if(min != null && root.data < min || max != null && root.data > max) return false;
+        return validateBstWithLimit(root.left, min, root.data) && validateBstWithLimit(root.right, root.data, max);
+    }
+
+    /*public static boolean validateBstWithLimit(TreeNode root, int min, int max){
         if(root == null) return true;
         if(root.left == null && root.right == null){
             if(root.data >= min && root.data < max){
@@ -16,5 +23,5 @@ public class BstValidation{
             }
         }
         return validateBstWithLimit(root.left, min, root.data) && validateBstWithLimit(root.right, root.data, max);
-    }
+    }*/
 }
